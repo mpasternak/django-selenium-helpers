1d7306c1a45c8c2bdb4cc47e86ed0b69da9a4c70
Michał Pasternak <michal.dtz@gmail.com>
2013-04-13 21:19:22 +0200

    Initial import

---
+++ selenium_helpers/__init__.py
@@ -0,0 +1,147 @@
+# -*- encoding: utf-8 -*-
+
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
+from selenium.common.exceptions import InvalidSelectorException
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+
+WEB_DRIVER = webdriver.Firefox
+#WEB_DRIVER = webdriver.PhantomJS
+
+
+def MyWebDriver(base, **kwargs):
+    return type('MyWebDriver', (_MyWebDriver, base), kwargs)
+
+
+def wd():
+    return MyWebDriver(WEB_DRIVER)
+
+
+class MyWebElement(WebElement):
+    """WebElement with some support for jquery selectors.
+    """
+
+    def __repr__(self):
+        """Return a pretty name for an element"""
+
+        id = self.get_attribute('id')
+        class_ = self.get_attribute('class')
+
+        if len(id) > 0:
+            return '#' + id
+        elif len(class_) > 0:
+            return '.'.join([self.tag_name] + class_.split(' '))
+        else:
+            return self.tag_name
+
+    def children(self):
+        """Same as $(elem).children()
+        """
+        return self.parent.execute_script(
+            '''return $(arguments[0]).children().get();''', self)
+
+    def jq_parent(self):
+        """Same as $(elem).parent()
+        """
+        return self.parent.execute_script(
+            '''return $(arguments[0]).parent().get();''', self)[0]
+
+    def val(self):
+        """Same as $(elem).val()
+        """
+        return self.parent.execute_script(
+            '''return $(arguments[0]).val();''', self)
+
+    def find_elements_by_jquery(self, jq):
+        """Same as $(elem).find(...)
+        """
+        return self.parent.execute_script(
+            '''return $(arguments[0]).find('%s').get();''' % jq, self)
+
+    def jq_is(self, arg):
+        """Same as $(elem).is(...)
+        """
+        return self.parent.execute_script(
+            '''return $(arguments[0]).is('%s');''' % arg, self)
+
+    def visible(self):
+        return self.jq_is(":visible")
+
+    def hidden(self):
+        return self.jq_is(":hidden")
+
+    def find_element_by_jquery(self, jq):
+        """Find exactly one element by jquery.
+        """
+        elems = self.find_elements_by_jquery(jq)
+        if len(elems) == 1:
+            return elems[0]
+        else:
+            raise InvalidSelectorException(
+                "jQuery selector returned %i elements, expected 1" % len(elems))
+
+
+class _MyWebDriver(object):
+    def create_web_element(self, element_id):
+        return MyWebElement(self, element_id)
+
+    def find_elements_by_jquery(self, jq):
+        return self.execute_script('''return $('%s').get();''' % jq)
+
+    def find_element_by_jquery(self, jq):
+        elems = self.find_elements_by_jquery(jq)
+        if len(elems) == 1:
+            return elems[0]
+        else:
+            raise InvalidSelectorException(
+                "jQuery selector returned %i elements, expected 1" % len(elems))
+
+    def wait_for_selector(self, selector, displayed=None):
+        def f(selenium):
+            element = selenium.find_element_by_css_selector(selector)
+            if displayed is not None:
+                return element.is_displayed() == displayed
+            return True
+
+        WebDriverWait(self, 10).until(
+            lambda driver: f(driver))
+
+
+def get_selected_option(field):
+    """Returns first selected <option> tag from a <select> field
+
+    :param field:
+    :ptype field: selenium.webdriver.remote.WebElement
+    """
+
+    for element in field.find_elements_by_tag_name('option'):
+        if element.is_selected():
+            return element
+
+
+def select_option_by_text(field, value):
+    """Selects an option from a <select> field basing on its text value,
+    <option value="something">THE VALUE PASSED TO THIS FUNCTION</option>
+
+    :param field: SELECT tag
+    :param value: value, that should be set
+    :ptype value: str
+    """
+
+    for el in field.find_elements_by_tag_name('option'):
+        if el.text == value:
+            el.click()
+            return
+
+
+class SeleniumTestCase(LiveServerTestCase):
+    """
+    A base test case for Selenium, providing hepler methods for generating
+    clients and logging in profiles.
+    """
+
+    def open(self, url):
+        self.page.get("%s%s" % (self.live_server_url, url))
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='django-selenium-helpers',
+      version='1.0',
+      description='Helpers for Django testing with selenium',
+      packages=['selenium_helpers']
+     )
